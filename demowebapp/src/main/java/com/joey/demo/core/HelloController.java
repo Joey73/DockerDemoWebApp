d5bc3f5986c6060e109386115ec7f0783eb1f01d
Joerg Schmitz <schmitz873@gmail.com>
2017-05-18 00:29:52 +0200

    Initial commit

---
+++ demowebapp/src/main/java/com/joey/demo/core/HelloController.java
@@ -0,0 +1,12 @@
+package com.joey.demo.core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/")
+	public String hello(){
+		return "Hello world";
+	}
+}
